9ede4b5c9aef02efffd68192c1df80d714340183
SONLE03 <sonle102003@gmail.com>
2023-12-22 02:01:23 +0700

    GSOP-197 Update style guide in style file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,10 @@
 import type { Config } from 'tailwindcss'
 
-const generateColor = (name: string, colors: number[]) => {
-	return colors.reduce(
-		(obj, num) => ({ ...obj, [num]: `var(--${name}-${num})` }),
-		{}
-	);
+const generateColor = (name: string, colors: number[] | string[]) => {
+    return colors.reduce(
+        (obj, num) => ({ ...obj, [num]: `var(--${name}-${num})` }),
+        {},
+    );
 };
 
 const config: Config = {
@@ -14,23 +14,54 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-		extend: {
-			colors: {
-				primary: generateColor(
-					"primary",
-					[50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 950]
-				),
-				secondary: generateColor(
-					"secondary",
-					[50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 950]
-				),
-				error: generateColor(
-					"error",
-					[50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 950]
-				),
+	extend: {
+		colors: {
+			primary: generateColor(
+				"primary",
+				[50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 950],
+			),
+			secondary: generateColor(
+				"secondary",
+				[
+					25, 50, 75, 100, 200, 300, 400, 500, 600, 700, 800, 900,
+					950,
+				],
+			),
+			red: generateColor(
+				"red",
+				[50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 950],
+			),
+			green: generateColor(
+				"green",
+				[50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 950],
+			),
+			yellow: generateColor(
+				"yellow",
+				[50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 950],
+			),
+			color: generateColor("color", [
+				"heading",
+				"body",
+				"notation",
+				"error",
+			]),
+			surface: generateColor("surface", [
+				"white",
+				"grey01",
+				"grey02",
+				"blue",
+				"dark",
+				"disabled",
+			]),
+			line: generateColor("line", ["grey01", "grey02", "blue"]),
+			background: {
+				normal: "var(--background)",
+				hover: "var(--background-hover)",
+				active: "var(--background-active)",
 			},
 		},
 	},
+},
   plugins: [require("flowbite/plugin")],
 }
 export default config
