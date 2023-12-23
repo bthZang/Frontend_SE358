d18109554b22b5521f7d19c0eb15b1bd633c696a
nvtuanqt212uit <winqti@gmail.com>
2023-12-23 07:54:32 +0700

    GSOP-195 config interceptor

---
+++ src/constant/apiEndpoint.ts
@@ -0,0 +1,8 @@
+const BASE_API = "http://localhost:8080";
+
+export const API = {
+  baseUrl: BASE_API,
+  authentication: {
+    signIn: `${BASE_API}/auth/authenticate`,
+  },
+};
---
+++ src/utils/fetcher.ts
@@ -0,0 +1,15 @@
+export async function publicFetcher(
+  url: string,
+  method: "GET" | "POST" | "PUT" | "DELETE",
+  body: any
+) {
+  const res = await fetch(url, {
+    method,
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+
+  return res;
+}
