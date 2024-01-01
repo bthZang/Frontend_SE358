173b5d289eb399433b0d35fcc72e6d0265bbb012
Zang <bt.hoanggiang@gmail.com>
2024-01-02 00:08:54 +0700

    dev: add cookies constant (GSOP-228)

--- src/constants/apiEndpoint.ts
+++ src/constants/apiEndpoint.ts
@@ -1,8 +1,13 @@
-const BASE_API = "http://localhost:8080";
+const BASE_API = "https://esms.hoanghy.space";
 
-export const API = {
-  baseUrl: BASE_API,
-  authentication: {
-    signIn: `${BASE_API}/auth/authenticate`,
-  },
+const API = {
+    baseUrl: BASE_API,
+    authentication: {
+        signIn: `${BASE_API}/auth/authenticate`,
+    },
+    staff: {
+        getStaffProfile: `${BASE_API}/staff/profile`,
+    },
 };
+
+export default API;
