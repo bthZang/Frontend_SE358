fd88d98b1bc643fb7594d99cc94a8982fcc7374a
Zang <bt.hoanggiang@gmail.com>
2024-01-03 01:24:20 +0700

    add api function for add new product (GSOP-209)

---
+++ src/api/product/addNewProduct.api.ts
@@ -0,0 +1,20 @@
+import apiInstance from "../apiInstance";
+
+export default async function addNewProduct(product: NewProduct) {
+    const response = await apiInstance.post("/product", product, {
+        headers: {
+            "Content-Type": "multipart/form-data",
+        },
+    });
+
+    return response.data;
+}
+
+export type NewProduct = {
+    name: string;
+    categoryId?: string;
+    price: number;
+    unit: string;
+    warrantyPeriod: number;
+    photo?: File | null;
+};
