48d1ba3cc3b4fd8383acc764053ddd996e6ef9ce
nvtuanqt212uit <winqti@gmail.com>
2024-01-02 21:48:46 +0700

    GSOP-207 add filter component

---
+++ src/components/Filter/FilterUI.stories.ts
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import Filter from "./FilterUI";
+import withQuery from "../../utils/withQuery";
+import SEARCH_PARAMS from "../../constants/searchParams";
+
+const meta = {
+    title: "Components/Filter",
+    component: Filter,
+    parameters: {
+        layout: "centered",
+    },
+    tags: ["autodocs"],
+    argTypes: {},
+} satisfies Meta<typeof Filter>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+    args: {
+        title: "Category",
+        items: ["Category 1", "Category 2"],
+    },
+    parameters: {
+        nextjs: {
+            appDirectory: true,
+            navigation: {
+                pathname: withQuery("/product", {
+                    [SEARCH_PARAMS.productName]: "Name",
+                }),
+            },
+        },
+    },
+};
