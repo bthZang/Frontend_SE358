ebc8d8c8003ec30c8a09b95d826649c76fba9976
SONLE03 <sonle102003@gmail.com>
2024-01-04 22:11:57 +0700

    GSOP-200 update product search component

--- src/components/ProductSearch/ProductSearchUI.tsx
+++ src/components/ProductSearch/ProductSearchUI.tsx
@@ -5,14 +5,15 @@ import {
     Spinner,
     TextInput as TextInputFlowbite,
 } from "flowbite-react";
-import { HiOutlineCheck } from "react-icons/hi";
+import { HiOutlineCheck, HiOutlineSearch } from "react-icons/hi";
 
 import Category from "@/types/entity/Category";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import React, { useRef, useState } from "react";
 import { useDeepCompareEffect } from "react-use";
 import SEARCH_PARAMS from "../../constants/searchParams";
-
+import withQuery from "../../utils/withQuery";
+import Button from "../Button/Button";
 
 export default function ProductSearchUI({
     onCategoryChange = () => {},
@@ -23,7 +24,6 @@ export default function ProductSearchUI({
     isProductLoading = false,
     categories = [],
     ...props
-
 }: PropTypes) {
     const router = useRouter();
     const pathname = usePathname();
@@ -35,12 +35,13 @@ export default function ProductSearchUI({
     useDeepCompareEffect(() => {
         onCategoryChange(category);
     }, [category]);
-return (
+
+    return (
         <div {...props}>
             <ButtonFlowbite.Group>
                 <Dropdown
                     theme={dropdownTheme}
-                    label={category?.name || "All"}
+                    label={category?.name || "All categories"}
                     dismissOnClick={true}
                     onClick={onCategoryDropdownClicked}
                     size="sm"
@@ -70,6 +71,15 @@ return (
                                     </p>
                                 </Dropdown.Item>
                             ))}
+                            <Dropdown.Item
+                                theme={dropdownTheme?.floating?.item}
+                                onClick={() => setCategory(undefined)}
+                                icon={category ? null : HiOutlineCheck}
+                            >
+                                <p className="  w-full text-start font-medium text-primary-300">
+                                    All categories
+                                </p>
+                            </Dropdown.Item>
                         </>
                     )}
                 </Dropdown>
@@ -82,17 +92,67 @@ return (
                     placeholder="Enter product name here..."
                     sizing="md"
                 />
+                <Button
+                    size="sm"
+                    pill
+                    isLoading={isProductLoading}
+                    onClick={() => {
+                        router.push(
+                            withQuery(pathname, {
+                                [SEARCH_PARAMS.productName]:
+                                    productNameRef.current?.value,
+                                [SEARCH_PARAMS.categoryName]: category?.name,
+                            }),
+                        );
+                    }}
+                >
+                    <HiOutlineSearch className=" h-4 w-4" />
+                </Button>
             </ButtonFlowbite.Group>
         </div>
     );
 }
 
 const dropdownTheme: CustomFlowbiteTheme["dropdown"] = {
-   
+    arrowIcon: "ml-2 h-4 w-4 text-secondary-950",
+    content: "py-1 text-secondary-900 focus:outline-none",
+    floating: {
+        animation: "transition-opacity",
+        arrow: {
+            base: "absolute z-10 h-2 w-2 rotate-45",
+            style: {
+                light: "bg-secondary-900",
+                auto: "bg-secondary-900",
+            },
+            placement: "-4px",
+        },
+        base: "z-10 w-fit rounded divide-y divide-gray-100 shadow focus:outline-none",
+        content: "py-1 text-sm text-gray-700",
+        divider: "my-1 h-px bg-gray-100 dark:bg-gray-600",
+        header: "block py-2 px-4 text-sm text-gray-700 dark:text-gray-200",
+        hidden: "invisible opacity-0",
+        item: {
+            container: " w-[180px]",
+            base: "flex flex-row-reverse items-center justify-between py-2 px-4 text-sm text-secondary-800 cursor-pointer w-full hover:bg-secondary-100 focus:bg-secondary-100 ",
+            icon: "mr-2 justify-self-end h-5 w-5",
+        },
+        style: {
+            light: "border border-secondary-200 bg-white text-secondary-900",
+            auto: "border border-secondary-200 bg-white text-secondary-900",
+        },
+        target: " w-fit bg-primary-100 text-secondary-950 transition duration-200 enabled:hover:bg-primary-200 enabled:active:bg-primary-300",
+    },
+    inlineWrapper: "flex items-center justify-between",
 };
 
 const textInputTheme: CustomFlowbiteTheme["textInput"] = {
-   
+    field: {
+        input: {
+            withAddon: {
+                off: "rounded-none w-[240px]",
+            },
+        },
+    },
 };
 
 type PropTypes = React.ComponentPropsWithRef<"div"> & {
