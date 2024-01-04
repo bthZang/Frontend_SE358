9db25f9bb8c2df1d33c5b4b1e8580bd4a2f86086
SONLE03 <sonle102003@gmail.com>
2024-01-04 22:34:31 +0700

    GSOP-200 apply stories for product search component

---
+++ src/components/ProductSearch/ProductSearchUI.stories.tsx
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import ProductSearch from "./ProductSearchUI";
+import withQuery from "../../utils/withQuery";
+import SEARCH_PARAMS from "../../constants/searchParams";
+import Category from "@/types/entity/Category";
+
+const meta = {
+    title: "Components/Item/ProductSearch",
+    component: ProductSearch,
+    parameters: {
+        layout: "centered",
+    },
+    tags: ["autodocs"],
+    argTypes: {
+        isCategoryLoading: { control: "boolean" },
+        isProductLoading: { control: "boolean" },
+    },
+} satisfies Meta<typeof ProductSearch>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+    args: {
+        categories: [
+            { id: "123", name: "CPU" },
+            { id: "223", name: "RAM" },
+        ] as Category[],
+    },
+    parameters: {
+        nextjs: {
+            appDirectory: true,
+            navigation: {
+                pathname: withQuery("/product", {
+                    [SEARCH_PARAMS.productName]: "Name",
+                }),
+            },
+        },
+    },
+};
