f31945624c8a5e0de6505378ce4e5b499fa7a976
SONLE03 <sonle102003@gmail.com>
2024-01-13 23:16:26 +0700

    GSOP-297 add category search component

--- src/components/CategorySearch/CategorySearch.tsx
+++ src/components/CategorySearch/CategorySearch.tsx
@@ -1,21 +1,7 @@
-import viewCategoryList from "@/api/category/viewCategoryList.api";
-import { useQuery } from "react-query";
 import CategorySearchUI from "./CategorySearchUI";
 
 export default function CategorySearch(
     props: Omit<React.ComponentPropsWithoutRef<"div">, "onClick">,
 ) {
-    const { data: categories, isLoading: isCategoryLoading } = useQuery(
-        ["category"],
-        viewCategoryList,
-        {},
-    );
-
-    return (
-        <CategorySearchUI
-            isCategoryLoading={isCategoryLoading}
-            categories={categories}
-            {...props}
-        ></CategorySearchUI>
-    );
+    return <CategorySearchUI {...props}></CategorySearchUI>;
 }
