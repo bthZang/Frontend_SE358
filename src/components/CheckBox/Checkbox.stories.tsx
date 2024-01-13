df501c73828cbb74de2a64be7c7ac7a06ad5c33c
SONLE03 <sonle102003@gmail.com>
2024-01-13 22:24:48 +0700

    GSOP-298 update title checkbox story

--- src/components/CheckBox/Checkbox.stories.tsx
+++ src/components/CheckBox/Checkbox.stories.tsx
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 import CheckBox from "./CheckBox";
 
 const meta = {
-	title: "Components/CheckBox",
+	title: "Components/Item/CheckBox",
 	component: CheckBox,
 	parameters: {
 		layout: "centered",
