e9f3908fa5a7d9de0b59a66d15018059e5fcce0f
SONLE03 <sonle102003@gmail.com>
2024-01-15 07:33:56 +0700

    GSOP-300 add category form and category form modal

--- src/components/CreateCategoryForm/CreateCategoryForm.tsx
+++ src/components/CreateCategoryForm/CreateCategoryForm.tsx
@@ -1,8 +1,64 @@
 "use client";
-export default function CreateCategoryForm() {
-    return (
-       <div>
 
-       </div>
+import viewCategoryList from "@/api/category/viewCategoryList.api";
+import useLoading from "@/hooks/useLoading";
+import Category from "@/types/entity/Category";
+import React from "react";
+import { useMutation, useQuery } from "react-query";
+import { useCreateCategoryModal } from "./CreateCategoryFormModal";
+import toast from "react-hot-toast";
+import OperationStateToast from "../OperationStateToast/OperationStateToast";
+import CreateCategoryFormUI from "./CreateCategoryFormUI";
+import addNewCategory from "@/api/category/addNewCategory.api";
+
+export default function CreateCategoryForm() {
+    const { data: categories, isLoading: isCategoriesLoading } = useQuery<
+        Category[]
+    >(["category"], viewCategoryList);
+    const { openLoading, closeLoading } = useLoading();
+
+    const { refetchCategoryList, closeCreateCategoryModal } =
+        useCreateCategoryModal();
+
+    const { mutate } = useMutation(addNewCategory, {
+        onMutate: () => {
+            openLoading("Creating Category...");
+        },
+        onSettled: () => {
+            closeLoading();
+        },
+        onSuccess: (_, data) => {
+            refetchCategoryList?.();
+            toast.custom(
+                (t) => (
+                    <OperationStateToast
+                        isSuccess
+                        content="Creating Category successfully"
+                        t={t}
+                    />
+                ),
+                { duration: 3000 },
+            );
+            closeCreateCategoryModal();
+        },
+        onError: (error: any, data) => {
+            toast.custom(
+                (t) => (
+                    <OperationStateToast
+                        isSuccess={false}
+                        t={t}
+                        title={error.message}
+                        content="Fail to create Category"
+                        retry={() => mutate(data)}
+                    />
+                ),
+                { duration: 3000 },
+            );
+        },
+    });
+
+    return (
+      <div>
+      </div>
     );
 }
--- src/components/CreateCategoryForm/CreateCategoryFormModal.tsx
+++ src/components/CreateCategoryForm/CreateCategoryFormModal.tsx
@@ -1,7 +1,36 @@
+import { ModalStateContext } from "@/contexts/ModalContext";
+import { Modal } from "flowbite-react";
+import { useContext } from "react";
+import CreateCategoryForm from "./CreateCategoryForm";
+
 export function useCreateCategoryModal() {
-  
+    const {
+        modalState: { addCategory },
+        setModalState,
+    } = useContext(ModalStateContext);
+
+    return {
+        isCreateCategoryModalOpen: addCategory.isOpen,
+        refetchCategoryList: addCategory.refetch,
+        openCreateCategoryModal: (refetch?: () => any) =>
+            setModalState({ addCategory: { isOpen: true, refetch } }),
+        closeCreateCategoryModal: () =>
+            setModalState({ addCategory: { isOpen: false } }),
+    };
 }
 
 export default function CreateCategoryFormModal() {
-    
+    const { isCreateCategoryModalOpen, closeCreateCategoryModal } =
+        useCreateCategoryModal();
+
+    return (
+        <Modal
+            theme={{ content: { inner: " rounded-2xl" } }}
+            size="3xl"
+            show={isCreateCategoryModalOpen}
+            onClose={closeCreateCategoryModal}
+        >
+            <CreateCategoryForm />
+        </Modal>
+    );
 }
