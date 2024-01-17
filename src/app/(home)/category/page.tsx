13fab22858d1cef3241406880cc37f166b04762b
SONLE03 <sonle102003@gmail.com>
2024-01-18 00:19:41 +0700

    GSOP-319 update datatable and add permission for category

--- src/app/(home)/category/page.tsx
+++ src/app/(home)/category/page.tsx
@@ -13,6 +13,7 @@ import DataTable from "@/components/DataTable/DataTable";
 import CategorySearch from "@/components/CategorySearch/CategorySearch";
 import { useDeleteCategoryMutation } from "@/api/category/deleteCategory.api";
 import { useUpdateCategoryModal } from "@/components/UpdateCategoryForm/UpdateCategoryFormModal";
+import { usePermission } from "@/hooks/usePermission";
 
 export default function Page() {
     const searchParams = useSearchParams();
@@ -33,22 +34,27 @@ export default function Page() {
 
     const deleteCategoryMutation = useDeleteCategoryMutation(refetch);
 
+    const isAllowedCreate = usePermission("CATEGORY", ["CREATE"]);
+
     return (
         <>
             <div className=" w-full grid grid-cols-2">
                 <CategorySearch className="" />
                 <div className=" flex justify-end gap-8">
-                    <Button
-                        size="md"
-                        onClick={() => openCreateCategoryModal(refetch)}
-                    >
-                        <HiPlus className=" w-4 h-4 mr-2" />
-                        New category
-                    </Button>
+                    {isAllowedCreate ? (
+                        <Button
+                            size="md"
+                            onClick={() => openCreateCategoryModal(refetch)}
+                        >
+                            <HiPlus className=" w-4 h-4 mr-2" />
+                            New category
+                        </Button>
+                    ) : null}
                 </div>
             </div>
             <DataTable
                 data={data || []}
+                entityType="CATEGORY"
                 isLoading={isLoading}
                 onDelete={(category) => {
                     openClaimModal(
--- src/components/DataTable/DataTable.tsx
+++ src/components/DataTable/DataTable.tsx
@@ -1,21 +1,21 @@
 "use client";
 
 import BaseEntity from "@/types/entity/BaseEntity";
-import { CustomFlowbiteTheme, Dropdown, Table } from "flowbite-react";
-import { HiOutlineDotsVertical, HiPencil, HiTrash } from "react-icons/hi";
-import Button from "../Button/Button";
+import { EntityType } from "@/types/entity/PermissionResponse";
 import FONT from "@/utils/fontFamily";
+import { CustomFlowbiteTheme, Table } from "flowbite-react";
 import TableSketon from "../TableSkeleton/TableSkeleton";
 
 export default function DataTable<T extends Object & BaseEntity>({
     data,
+    entityType,
     isLoading = false,
     pick,
     className,
     isEdit = true,
     onEdit,
     onDelete,
-    onClickRow = () => { },
+    onClickRow = () => {},
     ...props
 }: PropTypes<T>) {
     return (
@@ -23,9 +23,6 @@ export default function DataTable<T extends Object & BaseEntity>({
             className={`overflow-auto flex-1 max-w-full h-fit max-h-full rounded-lg border-[1px] border-secondary-200 ${className}`}
             {...props}
         >
-            {/* <p className="text-yellow-500 text-sm font-semibold mb-4">
-                {data?.length} items
-            </p> */}
             {isLoading ? (
                 <TableSketon col={Object.keys(pick).length} />
             ) : (
@@ -55,95 +52,6 @@ export default function DataTable<T extends Object & BaseEntity>({
                         )}
                     </Table.Head>
                     <Table.Body className="divide-y">
-                        {data.map((row, index) => (
-                            <Table.Row
-                                key={row.id}
-                                className="bg-white cursor-pointer hover:bg-background-hover duration-100"
-                                onClick={() => onClickRow(row)}
-                            >
-                                <Table.Cell
-                                    theme={{
-                                        base: "group-first/body:group-first/row:first:rounded-tl-lg group-first/body:group-first/row:last:rounded-tr-lg group-last/body:group-last/row:first:rounded-bl-lg group-last/body:group-last/row:last:rounded-br-lg px-4 py-4 text-center text-secondary-900 font-semibold",
-                                    }}
-                                >
-                                    {index + 1}
-                                </Table.Cell>
-                                {Object.keys(pick).map((column) => (
-                                    <Table.Cell
-                                        key={`${row.id}_${column}`}
-                                        theme={{
-                                            base: `${tableTheme?.body?.cell?.base
-                                                } ${pick[column as keyof typeof row]
-                                                    .className || ""
-                                                }`,
-                                        }}
-                                    >
-                                        {pick[column as keyof typeof row]
-                                            .editable ? (
-                                            <input
-                                                defaultValue={
-                                                    pick[
-                                                        column as keyof typeof row
-                                                    ].mapper?.(
-                                                        row[
-                                                        column as keyof typeof row
-                                                        ],
-                                                    ) ||
-                                                    (row[
-                                                        column as keyof typeof row
-                                                    ] as string)
-                                                }
-                                            />
-                                        ) : (
-                                            pick[
-                                                column as keyof typeof row
-                                            ].mapper?.(
-                                                row[column as keyof typeof row],
-                                            ) ||
-                                            (row[
-                                                column as keyof typeof row
-                                            ] as string)
-                                        )}
-                                    </Table.Cell>
-                                ))}
-                                {isEdit && (
-                                    <Table.Cell
-                                        theme={tableTheme?.body?.cell}
-                                        onClick={(e) => e.preventDefault()}
-                                    >
-                                        <Dropdown
-                                            label=""
-                                            renderTrigger={() => (
-                                                <div>
-                                                    <Button btnType="secondary">
-                                                        <HiOutlineDotsVertical className=" w-4 h-4" />
-                                                    </Button>
-                                                </div>
-                                            )}
-                                            dismissOnClick={false}
-                                        >
-                                            <Dropdown.Item
-                                                icon={HiPencil}
-                                                onClick={() => onEdit?.(row)}
-                                            >
-                                                Edit
-                                            </Dropdown.Item>
-                                            <Dropdown.Item
-                                                theme={{
-                                                    icon: " text-red-600 mr-2 h-4 w-4",
-                                                }}
-                                                icon={HiTrash}
-                                                onClick={() => onDelete?.(row)}
-                                            >
-                                                <p className=" text-red-600">
-                                                    Delete
-                                                </p>
-                                            </Dropdown.Item>
-                                        </Dropdown>
-                                    </Table.Cell>
-                                )}
-                            </Table.Row>
-                        ))}
                     </Table.Body>
                 </Table>
             )}
@@ -151,34 +59,34 @@ export default function DataTable<T extends Object & BaseEntity>({
     );
 }
 
-const tableTheme: CustomFlowbiteTheme["table"] = {
+export const tableTheme: CustomFlowbiteTheme["table"] = {
     root: {
-        base: "w-full text-left rounded-lg text-sm text-secondary-500",
-        shadow: "absolute bg-white dark:bg-black w-full h-full top-0 left-0 rounded-lg drop-shadow-md -z-10",
+        base: "min-w-full text-left rounded-lg text-sm text-secondary-500",
+        shadow: "absolute bg-background-secondary dark:bg-black w-full h-full top-0 left-0 rounded-lg drop-shadow-md -z-10",
         wrapper: "relative ",
     },
     body: {
-        base: "group/body",
+        base: "group/body bg-background-secondary",
         cell: {
-            base: `group-first/body:group-first/row:first:rounded-tl-lg group-first/body:group-first/row:last:rounded-tr-lg group-last/body:group-last/row:first:rounded-bl-lg group-last/body:group-last/row:last:rounded-br-lg px-4 py-4 font-semibold text-secondary-900 ${FONT.primary.className}`,
+            base: ` group-first/body:group-first/row:first:rounded-tl-lg group-first/body:group-first/row:last:rounded-tr-lg group-last/body:group-last/row:first:rounded-bl-lg group-last/body:group-last/row:last:rounded-br-lg p-2 sm:p-3 md:p-4 font-normal text-secondary-900 ${FONT.primary.className}`,
         },
     },
     head: {
-        base: "group/head text-xs border-b-2 border-secondary-200 uppercase text-gray-700",
+        base: "group/head bg-background-secondary text-xs border-b-2 border-secondary-200 uppercase text-secondary-700",
         cell: {
-            base: "group-first/head:first:rounded-tl-lg border-b-[1px] border-secondary-200  group-first/head:last:rounded-tr-lg bg-gray-50 dark:bg-gray-700 px-4 py-4",
+            base: "  group-first/head:first:rounded-tl-lg border-b-[1px] border-secondary-200  group-first/head:last:rounded-tr-lg p-2 sm:p-3 md:p-4 sm:p-2 md:p-4",
         },
     },
     row: {
-        base: "group/row",
-        hovered: "hover:bg-secondary-100",
-        striped:
-            "odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700",
+        base: "group/row bg-background-secondary",
+        hovered: "hover:bg-primary-100",
+        striped: "odd:bg-background-secondary even:bg-background-secondary ",
     },
 };
 
 type PropTypes<T> = {
     data: T[];
+    entityType?: EntityType;
     isLoading?: boolean;
     onEdit?: (product: T) => any;
     onDelete?: (product: T) => any;
